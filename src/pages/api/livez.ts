cb2498b463bfd1ec7809942d76c2a2c6a0bca00a
Max Eliseev <mxseev@gmail.com>
2023-02-15 05:31:47 +0500

    feat: add HOSTNAME to /api/livez

--- src/pages/api/livez.ts
+++ src/pages/api/livez.ts
@@ -1,7 +1,7 @@
 import {NextApiHandler} from "next"
 
 const livez: NextApiHandler = (_request, response) => {
-  return response.json({status: "ok"})
+  return response.json({status: "ok", hostname: process.env.HOSTNAME})
 }
 
 export default livez
